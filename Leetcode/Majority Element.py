5cf9744398a854ee05d045b3b0ba87703183cf80
Roy Lu <Roylu0404@gmail.com>
2025-12-30 21:48:08 -0500

    Majority Element

---
+++ Leetcode/Majority Element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = Counter(nums)
+        return count.most_common(1)[0][0]
+        
