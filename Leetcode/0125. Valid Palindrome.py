d5f47a3507df9a6bdf5ca6eea69c3549f9b98256
Roy Lu <Roylu0404@gmail.com>
2026-01-15 22:52:11 -0500

    Create 0125. Valid Palindrome.py

---
+++ Leetcode/0125. Valid Palindrome.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        palindrome = (''.join(char for char in s if char.isalpha() or char.isnumeric())).lower()
+        return palindrome == palindrome[::-1]
